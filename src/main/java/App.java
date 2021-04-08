ee3ff358158d7a0ca22497c60213cd18b5053bbe
Yuriy <39201286+yuri2711@users.noreply.github.com>
2021-04-08 23:31:35 +0300

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat)applicationContext.getBean("cat");
+        System.out.println(bean.getMessage());
+    }
+}
